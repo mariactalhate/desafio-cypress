78352b8915759febcff3fadd57293465f90c3bb9
Maria Talhate <maria.talhate@picpay.com>
2025-03-18 18:09:07 -0300

    remove node_modules and add gitignore

--- cypress/e2e/backend.cy.js
+++ cypress/e2e/backend.cy.js
@@ -1,4 +1,9 @@
 describe('template spec', () => {
+
+  before(() => {
+    Cypress.config('baseUrl', 'https://serverest.dev/')
+  })
+
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -1,4 +1,8 @@
 describe('template spec', () => {
+  before(() => {
+    Cypress.config('baseUrl', 'https://front.serverest.dev')
+  })
+  
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
